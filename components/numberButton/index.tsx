23c73994caf49a5ffc071d964f8929b117d48155
Livia Devolder <liviadevolder@gmail.com>
2022-08-03 15:13:51 -0300

    feat: implement funcional number buttons

--- components/numberButton/index.tsx
+++ components/numberButton/index.tsx
@@ -1,11 +1,20 @@
+import { useContext } from "react";
+import MyContext from "../../context/myContext";
 import style from "./NumberButton.module.scss"
 
 export default function NumberButton() {
+  const { setSumValue } = useContext(MyContext);
+  const defaultValues = [100, 350, 700]
+
+  const handleClick = event => {
+    setSumValue(event.target.value)
+  }
+
   return (
     <>
-      <button className={style.numberButton}>100</button>
-      <button className={style.numberButton}>350</button>
-      <button className={style.numberButton}>700</button>
+      {defaultValues.map(value => {
+        return <button value={value} onClick={handleClick} key={value} className={style.numberButton}>{value}</button>
+      })}
     </>
   );
 }
