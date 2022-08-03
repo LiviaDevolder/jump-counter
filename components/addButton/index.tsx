ea20149ba3e96544c9a02f686b6c72e87c545af6
Livia Devolder <liviadevolder@gmail.com>
2022-08-03 15:24:04 -0300

    feat: add and remove buttons

--- components/addButton/index.tsx
+++ components/addButton/index.tsx
@@ -1,9 +1,22 @@
+import { useContext } from "react";
+import MyContext from "../../context/myContext";
 import style from "./AddButton.module.scss"
 
 export default function AddButton() {
+  const { sumValue, finalValue, setFinalValue } = useContext(MyContext);
+
+  const handleClick = () => {
+    if (finalValue + sumValue > 99999) {
+      setFinalValue(99999)
+      alert("Parabéns! Você atingiu o limite de 99999 pulos.");
+    } else {
+      setFinalValue(finalValue + sumValue)
+    }
+  }
+
   return (
     <>
-      <button className={style.addButton}>Add</button>
+      <button onClick={handleClick} className={style.addButton}>Add</button>
     </>
   )
 }
--- components/removeButton/index.tsx
+++ components/removeButton/index.tsx
@@ -1,9 +1,23 @@
+import { useContext } from "react";
+import MyContext from "../../context/myContext";
 import style from "./RemoveButton.module.scss";
 
 export default function RemoveButton() {
+  const { sumValue, finalValue, setFinalValue } = useContext(MyContext);
+
+  const handleClick = () => {
+    if (finalValue - sumValue < 0) {
+      alert("Insira um valor menor ou igual ao número de pulos atual")
+    } else {
+      setFinalValue(finalValue - sumValue)
+    }
+  };
+
   return (
     <>
-      <button className={style.removeButton}>Remove</button>
+      <button onClick={handleClick} className={style.removeButton}>
+        Remove
+      </button>
     </>
   );
 }
